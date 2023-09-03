4675ba91c74f246b7ca07cc026dc941fb18dba26
Nexadis <37233902+Nexadis@users.noreply.github.com>
2023-09-03 16:27:58 +0300

    Сервер хранилища

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main(){
+import (
+	"log"
 
+	"github.com/Nexadis/Storage/internal/config"
+	"github.com/Nexadis/Storage/internal/server"
+)
+
+func main() {
+	c := config.New()
+	s := server.New(c)
+	log.Fatal(s.Run())
 }
