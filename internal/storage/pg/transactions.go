4ae91ba330a3c9eecd503957b1727ab659c6d019
Nexadis <37233902+Nexadis@users.noreply.github.com>
2023-09-05 20:21:30 +0300

    Транзакции через PostgreSQL

---
+++ internal/storage/pg/transactions.go
@@ -0,0 +1,127 @@
+package pg
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+
+	"github.com/Nexadis/Storage/internal/storage"
+)
+
+var TransactionsScheme = `CREATE TABLE TRANSACTIONS(
+ID SERIAL PRIMARY KEY,
+TYPE INT NOT NULL,
+KEY VARCHAR(1024) NOT NULL,
+VALUE VARCHAR(1024) NOT NULL
+);
+`
+
+type PostgreTransactionLogger struct {
+	events chan<- storage.Event
+	errors <-chan error
+	db     *sql.DB
+}
+
+func (p *PostgreTransactionLogger) WritePut(key, value string) {
+	p.events <- storage.Event{
+		EventType: storage.EventPut,
+		Key:       key,
+		Value:     value,
+	}
+}
+
+func (p *PostgreTransactionLogger) WriteDelete(key string) {
+	p.events <- storage.Event{
+		EventType: storage.EventDelete,
+		Key:       key,
+	}
+}
+
+func (p *PostgreTransactionLogger) Err() <-chan error {
+	return p.errors
+}
+
+func (p *PostgreTransactionLogger) ReadEvents() (<-chan storage.Event, <-chan error) {
+	errors := make(chan error, 1)
+	events := make(chan storage.Event)
+
+	go func() {
+		var e storage.Event
+
+		defer close(errors)
+		defer close(events)
+
+		q, err := p.db.Prepare(`SELECT * FROM TRANSACTIONS`)
+		if err != nil {
+			errors <- err
+			return
+		}
+
+		rows, err := q.Query()
+		if err != nil {
+			errors <- err
+			return
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			err := rows.Scan(&e.ID, &e.EventType, &e.Key, &e.Value)
+			if err != nil {
+				errors <- err
+				return
+			}
+			events <- e
+		}
+
+		err = rows.Err()
+		if err != nil {
+			errors <- err
+			return
+		}
+	}()
+
+	return events, errors
+}
+
+func (p *PostgreTransactionLogger) Run() {
+	events := make(chan storage.Event, 16)
+	p.events = events
+
+	errors := make(chan error, 1)
+	p.errors = errors
+	q, err := p.db.Prepare("INSERT INTO TRANSACTIONS (\"TYPE\",\"KEY\",\"VALUE\") VALUES($1,$2,$3)")
+	if err != nil {
+		errors <- err
+		return
+	}
+	go func() {
+		for e := range events {
+			e := e
+			_, err := q.Exec(&e.EventType, &e.Key, &e.Value)
+			if err != nil {
+				errors <- err
+				return
+			}
+		}
+	}()
+}
+
+func NewPostgreTransactionLogger(conn string) (storage.TransactionLogger, error) {
+	db, err := sql.Open("pgx", conn)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	pl := &PostgreTransactionLogger{
+		db: db,
+	}
+	_, err = pl.db.Exec(TransactionsScheme)
+	if err != nil {
+		log.Printf("Error while scheme creating:", err)
+	}
+	return pl, nil
+}
