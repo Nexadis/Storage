85bee7a44ce89d414fcce76468fffc57d1889eea
Nexadis <37233902+Nexadis@users.noreply.github.com>
2023-09-11 08:56:55 +0300

    Настройка grpc сервера

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,6 +4,7 @@ import "flag"
 
 type Config struct {
 	Addr     string
+	GRPC     string
 	FileSave string
 	DBURI    string
 }
@@ -11,6 +12,7 @@ type Config struct {
 func New() *Config {
 	c := &Config{}
 	flag.StringVar(&c.Addr, "a", ":8080", "Address for HTTP server")
+	flag.StringVar(&c.GRPC, "g", ":50051", "Address for gRPC server")
 	flag.StringVar(&c.FileSave, "s", "transactions.log", "Filename for transactions file")
 	flag.StringVar(&c.DBURI, "d", "", "URI for db connect")
 	flag.Parse()
