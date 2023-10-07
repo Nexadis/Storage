b9d5fd88c7f3a4d7741d1835fb39b01d9c7b0ad3
Nexadis <37233902+Nexadis@users.noreply.github.com>
2023-10-07 11:59:25 +0300

    Включение функциональности при явном указании флага, а также при работе в локальной сети

--- internal/server/httpserver/cookies.go
+++ internal/server/httpserver/cookies.go
@@ -3,6 +3,7 @@ package httpserver
 import (
 	"net/http"
 
+	"github.com/Nexadis/Storage/internal/server/features"
 	"github.com/Nexadis/Storage/internal/storage"
 )
 
@@ -11,9 +12,12 @@ const (
 )
 
 func GetUser(r *http.Request) string {
-	c, err := r.Cookie(UserCookie)
-	if err != nil {
-		return storage.DefaultUser
+	if features.FeatureEnabled(features.Use_user, r) {
+		c, err := r.Cookie(UserCookie)
+		if err != nil {
+			return storage.DefaultUser
+		}
+		return c.Value
 	}
-	return c.Value
+	return storage.DefaultUser
 }
